dddf86141621b17f56b15ad6dbcbe93d2781c26b
August Shi <awshi2@illinois.edu>
2018-02-17 03:33:06 -0600

    cc4c0a520863 replay gib_eks

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/store/jwk/JwkSetConverter.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/store/jwk/JwkSetConverter.java
@@ -46,6 +46,7 @@ import static org.springframework.security.oauth2.provider.token.store.jwk.JwkAt
  *
  * @author Joe Grandja
  * @author Vedran Pavic
+ * @author Michael Duergner
  */
 class JwkSetConverter implements Converter<InputStream, Set<JwkDefinition>> {
 	private final JsonFactory factory = new JsonFactory();
@@ -92,10 +93,16 @@ class JwkSetConverter implements Converter<InputStream, Set<JwkDefinition>> {
 						attributes.put(attributeName, parser.getValueAsString());
 					}
 				}
+
+				JwkDefinition jwkDefinition = null;
 				JwkDefinition.KeyType keyType =
 						JwkDefinition.KeyType.fromValue(attributes.get(KEY_TYPE));
 				if (JwkDefinition.KeyType.RSA.equals(keyType)) {
-					JwkDefinition jwkDefinition = createRsaJwkDefinition(attributes);
+					jwkDefinition = this.createRsaJwkDefinition(attributes);
+				} else if (JwkDefinition.KeyType.EC.equals(keyType)) {
+					jwkDefinition = this.createEllipticCurveJwkDefinition(attributes);
+				}
+				if (jwkDefinition != null) {
 					if (!jwkDefinitions.add(jwkDefinition)) {
 						throw new JwkException("Duplicate JWK found in Set: " +
 								jwkDefinition.getKeyId() + " (" + KEY_ID + ")");
@@ -161,6 +168,62 @@ class JwkSetConverter implements Converter<InputStream, Set<JwkDefinition>> {
 
 		RsaJwkDefinition jwkDefinition = new RsaJwkDefinition(
 				keyId, publicKeyUse, algorithm, modulus, exponent);
+
+		return jwkDefinition;
+	}
+
+	/**
+	 * Creates an {@link EllipticCurveJwkDefinition} based on the supplied attributes.
+	 *
+	 * @param attributes the attributes used to create the {@link EllipticCurveJwkDefinition}
+	 * @return a {@link JwkDefinition} representation of an EC Key
+	 * @throws JwkException if at least one attribute value is missing or invalid for an EC Key
+	 */
+	private JwkDefinition createEllipticCurveJwkDefinition(Map<String, String> attributes) {
+		// kid
+		String keyId = attributes.get(KEY_ID);
+		if (!StringUtils.hasText(keyId)) {
+			throw new JwkException(KEY_ID + " is a required attribute for an EC JWK.");
+		}
+
+		// use
+		JwkDefinition.PublicKeyUse publicKeyUse =
+				JwkDefinition.PublicKeyUse.fromValue(attributes.get(PUBLIC_KEY_USE));
+		if (!JwkDefinition.PublicKeyUse.SIG.equals(publicKeyUse)) {
+			throw new JwkException((publicKeyUse != null ? publicKeyUse.value() : "unknown") +
+					" (" + PUBLIC_KEY_USE + ") is currently not supported.");
+		}
+
+		// alg
+		JwkDefinition.CryptoAlgorithm algorithm =
+				JwkDefinition.CryptoAlgorithm.fromHeaderParamValue(attributes.get(ALGORITHM));
+		if (algorithm != null &&
+				!JwkDefinition.CryptoAlgorithm.ES256.equals(algorithm) &&
+				!JwkDefinition.CryptoAlgorithm.ES384.equals(algorithm) &&
+				!JwkDefinition.CryptoAlgorithm.ES512.equals(algorithm)) {
+			throw new JwkException(algorithm.standardName() + " (" + ALGORITHM + ") is currently not supported.");
+		}
+
+		// x
+		String x = attributes.get(EC_PUBLIC_KEY_X);
+		if (!StringUtils.hasText(x)) {
+			throw new JwkException(EC_PUBLIC_KEY_X + " is a required attribute for an EC JWK.");
+		}
+
+		// y
+		String y = attributes.get(EC_PUBLIC_KEY_Y);
+		if (!StringUtils.hasText(y)) {
+			throw new JwkException(EC_PUBLIC_KEY_Y + " is a required attribute for an EC JWK.");
+		}
+
+		// crv
+		String curve = attributes.get(EC_PUBLIC_KEY_CURVE);
+		if (!StringUtils.hasText(curve)) {
+			throw new JwkException(EC_PUBLIC_KEY_CURVE + " is a required attribute for an EC JWK.");
+		}
+
+		EllipticCurveJwkDefinition jwkDefinition = new EllipticCurveJwkDefinition(
+				keyId, publicKeyUse, algorithm, x, y, curve);
 
 		return jwkDefinition;
 	}
